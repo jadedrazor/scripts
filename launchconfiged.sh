01a3c78135c9639624e02edd4ded22e930f0de9a
jadedrazor <bjones@jadedrazor.com>
2014-03-14 15:31:05 +0000

    new launch configurator

--- launchconfiged.sh
+++ launchconfiged.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
- set -xv
+  set -xv
 # TAGS =  Name, Project, Environment, Role, Project, CostCentre
 
 
@@ -9,9 +9,12 @@ AMI=$2
 INTTYPE=$3
 USERDATA=$4
 SG=$5
-PROJECT=$6
-ENV=$7
-CC=$7
+MIN=$6
+MAX=$7
+DESIRED=$7
+VPC=subnet-91dfdce5,subnet-c3fedd85
+AVI="eu-west-1a eu-west-1b"
+ELB=kp-10245-web-elb
 
 KEY=kaplan
 
@@ -19,4 +22,10 @@ BASE=`basename $0`
 
 
 echo $BASE LaunchConfigName AMI-instance-id Instance-Type "UserData(take's URI's)" SecurityGroups Project CostCentre
+
+
+echo "Trying to create launch config $LCNAME"
 aws autoscaling create-launch-configuration --launch-configuration-name "$LCNAME" --image-id="$AMI"  --instance-type "$INTTYPE" --user-data  "${USERDATA}" --security-groups $SG --key-name $KEY
+
+echo "Trying to create auto-scale group $LCNAME-asg"
+aws autoscaling   create-auto-scaling-group --launch-configuration-name "$LCNAME" --auto-scaling-group-name $LCNAME-asg --min-size $MIN --max-size $MAX --desired-capacity $DESIRED --availability-zones $AVI --vpc-zone-identifier $VPC --load-balancer-names $ELB

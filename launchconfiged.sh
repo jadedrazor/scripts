5d7e608e53b5255435a5b3172faa744e7bd2eff0
jadedrazor <bjones@jadedrazor.com>
2014-03-18 11:43:47 +0000

    update

--- launchconfiged.sh
+++ launchconfiged.sh
@@ -1,7 +1,46 @@
 #!/bin/bash
-#  set -xv
-BASE=`basename $0`
+# set -xv
+#
+# This script will create a launch configuration and attach that to a Auto Scale Group
+# requires a file (which can be empty) that adds user data that will be applied to all instances
+# Example file for user data
+#
+## #!/bin/bash
+## # TAGS =  Name, Project, Environment, Role, CostCentre
+## PROJECT=kplus
+## ENV=p
+## ROLE=web
+## CC=Nathan
+## INSTANCEID=$(curl -s 169.254.169.254/latest/meta-data/instance-id)
+## MY_IP=$(curl -s 169.254.169.254/latest/meta-data/local-ipv4)
+## DOMAINNAME=aws.kapintdc.com
+## 	HOSTNAME=$PROJECT-10245-$ENV-$ROLE-$INSTANCEID
+## 	hostname $HOSTNAME
+## 	sed -i "s/HOSTNAME=.*/HOSTNAME=$HOSTNAME/" /etc/sysconfig/network
+## echo "$MY_IP $HOSTNAME.$DOMAINNAME $HOSTNAME" >> /etc/hosts
+## sed -i "s/search .*/search $DOMAINNAME/" /etc/resolv.conf
 
+# This script parses the included user data script to pull out information which is used for tagging the instances, the tags are the constants from the script ( PROJECT ENV ROLE CC )
+# hacked togethor by: Byron Jones
+# Orginally written: 17/03/2014
+# version 0.04
+
+#
+# When run you must supply 7 parameters
+# 1 LCNAME which is the name of the launch config, -asg is appended to this for the auto scale group
+# 2 AMI base AMI used for the instances
+# 3 Instance type
+# 4 User data file to include
+# 5 security groups to attach to the instance
+# 6 Minimum instances
+# 7 Maximum instances
+#
+# This script also requires 4 constants to be set
+# VPC contains a comma seperated list with the subnets the instances can be places in
+# ELB the elastic load balancer to attach the instances to
+# KEY the ssh key to attach to the instances
+
+BASE=`basename $0`
 
 LCNAME=$1
 AMI=$2
